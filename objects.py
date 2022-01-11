cc2f248152b2a37995ae78a165cbe99565aa0f38
bastienlc <bastien.le-chenadec@eleves.enpc.fr>
2022-01-11 15:30:34 +0100

    str methods for objects

--- objects.py
+++ objects.py
@@ -14,10 +14,22 @@ class Request:
             self.mate_id = mate_id
         self.shotgun_rank = shotgun_rank
 
+    def __str__(self):
+        return "\nstudent_id: " + str(self.student_id)\
+               + "\ngender: " + str(self.gender)\
+               + "\nscholarship: " + str(self.scholarship)\
+               + "\ndistance: " + str(self.distance)\
+               + "\nprefered_room_type: " + str(self.prefered_room_type)\
+               + "\naccept_other_type: " + str(self.accept_other_type)\
+               + "\nhas_mate: " + str(self.has_mate)\
+               + "\nmate_id: " + str(self.mate_id)\
+               + "\nshotgun_rank: " + str(self.shotgun_rank)
+
+
 
 class Room:
     def __init__(self, room_id, room_type):
-        self.id = room_id
+        self.room_id = room_id
         self.room_type = room_type
         if room_type == 0:  # chambre simple
             self.capacity = 1
@@ -38,6 +50,12 @@ class Room:
             print("binômée")
         else:
             print("double")
+
+    def __str__(self):
+        return "\nroom_type: " + str(self.room_type)\
+               + "\nroom_id: " + str(self.room_id)\
+               + "\ncapacity: " + str(self.capacity)\
+               + "\nstudents: " + str(self.students)
 
 
 class Attribution:
